052dfc957304bdbba9bbd5600db42e9be0496717
Grekhov1990 <grehoff1990@gmail.com>
2022-12-08 08:44:06 +0300

    Добавлены файлы вебсайта

---
+++ taskmanager/main/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Image(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return self.title
+
+class Task(models.Model):
+    title = models.CharField('Название', max_length=50)
+    task = models.TextField('Описание')
+
+    def __str__(self):
+        return self.title
